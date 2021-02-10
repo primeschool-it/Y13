f16a032cb14747ceef66e419ab327ce21f1ab2fd
yk2kus <yogeshkushwaha4@gmail.com>
2021-02-10 15:28:13 +0000

    finish airstrike game

--- airstrike.py
+++ airstrike.py
@@ -5,6 +5,7 @@ a greeb rect has to move left or right not not collide with red rect
 
 import pygame
 import random
+
 pygame.init()
 # Variables
 WIDTH = 1200
@@ -20,18 +21,39 @@ COLORS = [BLACK, WHITE, RED, GREEN]
 background_img = pygame.image.load("background.jpg")
 background_img = pygame.transform.scale2x(background_img)
 
+
+class Bullet(pygame.sprite.Sprite):
+    def __init__(self, x_pos, y_pos):
+        super(Bullet, self).__init__()
+        self.image = pygame.image.load('images/bullet.png')
+        self.image = pygame.transform.scale(self.image, (50, 50))
+        self.rect = self.image.get_rect()
+        self.rect.x = x_pos - (50 / 2)
+        self.rect.y = y_pos - 50
+
+    def update(self):
+        self.rect.y = self.rect.y - 3
+        if self.rect.y < 0:
+            self.kill()
+        ## destroy the bullet if leaves screen
+
+
+bullet_group = pygame.sprite.Group()
+
+
 class Spaceship(pygame.sprite.Sprite):
     def __init__(self):
         super(Spaceship, self).__init__()
         self.image = pygame.image.load('images/spaceship.png')
         self.rect = self.image.get_rect()
-        self.rect.x = WIDTH/2
+        self.rect.x = WIDTH / 2
         self.rect.y = HEIGHT - 70
 
 
 spaceship_group = pygame.sprite.Group()
 spaceship = Spaceship()
 spaceship_group.add(spaceship)
+
 
 class EnemyAsteroid(pygame.sprite.Sprite):
     def __init__(self, x_pos, y_pos, width, height, color):
@@ -42,57 +64,72 @@ class EnemyAsteroid(pygame.sprite.Sprite):
         self.rect.x = x_pos
         self.rect.y = y_pos
 
-
-
     def update(self):
-        print("self...........", self.name)
         box_width = self.rect.right - self.rect.left
         self.rect.y = self.rect.y + self.speed
-        if self.name == 'BOX 17':
-            self.image.fill(BLUE)
-            self.speed = 5
         if self.rect.top > HEIGHT:
-            self.rect.x = random.randint(0,WIDTH)
+            self.rect.x = random.randint(0, WIDTH)
             self.rect.y = 0
-            ####SPEED
-            # print ("disappearing.........", self.name)
-            # self.kill()
-
+        if len(asteroid_group) < 5:
+            rand_x = random.randint(0, WIDTH)
+            rand_y = random.randint(0, HEIGHT)
+            rand_width = random.randint(20, 50)
+            rand_height = random.randint(20, 50)
+            rand_color = random.choice(COLORS)
+            rand_speed = random.randint(1, 2)
+            asteroid = EnemyAsteroid(rand_x, rand_y, rand_width, rand_height, rand_color)
+            asteroid.speed = rand_speed
+            asteroid_group.add(asteroid)
 
 asteroid_group = pygame.sprite.Group()
 for i in range(5):
     rand_x = random.randint(0, WIDTH)
     rand_y = random.randint(0, HEIGHT)
-    rand_width = random.randint(20,50)
+    rand_width = random.randint(20, 50)
     rand_height = random.randint(20, 50)
     rand_color = random.choice(COLORS)
-    rand_speed = random.randint(1,2)
-    asteroid = EnemyAsteroid(rand_x, rand_y, rand_width, rand_height,rand_color )
-    asteroid.name = "BOX %s" %i
+    rand_speed = random.randint(1, 2)
+    asteroid = EnemyAsteroid(rand_x, rand_y, rand_width, rand_height, rand_color)
+    asteroid.name = "BOX %s" % i
     asteroid.speed = rand_speed
     asteroid_group.add(asteroid)
 
-
-
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 exit_game = True
+fired = False
+crashed = False
+SCORE = 0
 while exit_game:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             exit_game = False
         ## List of all pressed keys
-    keys = pygame.key.get_pressed()
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_SPACE] and pygame.KEYDOWN and not fired:
+            bullet = Bullet(spaceship.rect.midtop[0], spaceship.rect.midtop[1])
+            bullet_group.add(bullet)
+            fired = True
+        if event.type == pygame.KEYUP:
+            fired = False
+
     ## change position of green on pressing left or right arrow key
     if keys[pygame.K_LEFT]:
-        if spaceship.rect.left  > 0:
-            spaceship.rect.x =  spaceship.rect.x - 10
+        if spaceship.rect.left > 0:
+            spaceship.rect.x = spaceship.rect.x - 10
     if keys[pygame.K_RIGHT]:
         right_border = spaceship.rect.right + 10
         if right_border < WIDTH:
             spaceship.rect.x = spaceship.rect.x + 10
-    if not pygame.sprite.spritecollide(spaceship, asteroid_group, False):
-        screen.blit(background_img, (0,0))
+    if pygame.sprite.groupcollide(bullet_group, asteroid_group,True, True):
+        SCORE = SCORE + 10
+        print("Score is  ....%s"%SCORE)
+    if pygame.sprite.spritecollide(spaceship, asteroid_group, False):
+        crashed = True
+    if not crashed and not pygame.sprite.spritecollide(spaceship, asteroid_group, False):
+        screen.blit(background_img, (0, 0))
         asteroid_group.draw(screen)
         spaceship_group.draw(screen)
+        bullet_group.draw(screen)
         asteroid_group.update()
+        bullet_group.update()
         pygame.display.update()
